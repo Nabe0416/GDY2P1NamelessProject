5ccd2e0b729b002f1dd7b05fe5b76056c08d29c2
der_nabe <34216014+Nabe0416@users.noreply.github.com>
2019-09-23 14:26:09 +0200

    Enemy viewing feature added

--- GDY2P1Group1/Assets/Scripts/CharacterMovement.cs
+++ GDY2P1Group1/Assets/Scripts/CharacterMovement.cs
@@ -7,18 +7,19 @@ public class CharacterMovement : MonoBehaviour
 {
     //SerializeField can visualize the variable below in the inspector
     [SerializeField]
-    private float MoveSpeed = 5;//Declare a float
+    private float MoveSpeed = 5;
 
-    private Rigidbody2D rb;//Declare a rigidbody2d, it's a component of a gameobject
-    private SpriteRenderer sr;
+    [SerializeField]
+    private GameObject characterSprite;
 
-    /// <summary>
-    /// This method will be run after the Awake method. Awake method will be run after this gameobject is loaded
-    /// </summary>
+    private Rigidbody2D rb;
+
+    //The direction that the character is facing.
+    private Vector2 dir;
+
     private void Start()
     {
         rb = this.gameObject.AddComponent<Rigidbody2D>();
-        sr = this.gameObject.GetComponent<SpriteRenderer>();
         AdjustRb();
     }
 
@@ -29,30 +30,8 @@ public class CharacterMovement : MonoBehaviour
         rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
     }
 
-    /// <summary>
-    /// This is the function which detects the input of player and react on it.
-    /// </summary>
     private void Movement()
     {
-        /**
-        if(Input.GetKey(KeyCode.W))
-        {
-            rb.MovePosition((Vector2)this.transform.position + new Vector2(0, MoveSpeed * Time.deltaTime));
-        }
-        if(Input.GetKey(KeyCode.S))
-        {
-            rb.MovePosition((Vector2)this.transform.position + new Vector2(0, -MoveSpeed * Time.deltaTime));
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.MovePosition((Vector2)this.transform.position + new Vector2(-MoveSpeed * Time.deltaTime, 0));
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.MovePosition((Vector2)this.transform.position + new Vector2(MoveSpeed * Time.deltaTime, 0));
-        }
-        **/
-
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
@@ -66,14 +45,17 @@ public class CharacterMovement : MonoBehaviour
 
     private void Update()
     {
-        transform.up = ((GetMousePosition() - (Vector2)transform.position).normalized);
+        dir = (GetMousePosition() - (Vector2)transform.position).normalized;
+        characterSprite.transform.up = dir;
     }
 
-    /// <summary>
-    /// This method will be call per frame, after method Update is called.
-    /// </summary>
     private void FixedUpdate()
     {
         Movement();
+    }
+
+    public Transform GetPlayerPosition()
+    {
+        return transform;
     }
 }
---
+++ GDY2P1Group1/Assets/Scripts/ViewRange.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ViewRange : MonoBehaviour
+{
+    [SerializeField]
+    private TempAI ai;
+    private GameObject pc;
+    private GameObject tilemap;
+
+    private List<GameObject> list = new List<GameObject>();
+
+    private void Start()
+    {
+        pc = FindObjectOfType<CharacterMovement>().gameObject;
+        tilemap = FindObjectOfType<Tilemap>().gameObject;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject == pc)
+        {
+            RaycastHit2D[] results = Physics2D.RaycastAll(transform.position, (pc.transform.position - transform.position).normalized, Vector2.Distance(pc.transform.position, transform.position));
+            Debug.DrawRay(transform.position, pc.transform.position - transform.position, Color.yellow, 0.1f);
+
+            foreach (RaycastHit2D result in results)
+            {
+                if (list.Contains(result.collider.gameObject) == false)
+                {
+                    list.Add(result.collider.gameObject);
+                }
+            }
+            if (list.Contains(tilemap) == false && list.Count != 0)
+            {
+                ai.SetGoal(pc.GetComponent<CharacterMovement>().GetPlayerPosition());
+                ai.pcIsInView = true;
+            }
+            else
+            {
+                ai.SetGoal(null);
+                ai.pcIsInView = false;
+            }
+            list.Clear();//Can be optimized.
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        ai.SetGoal(null);
+        ai.pcIsInView = false;
+    }
+}
