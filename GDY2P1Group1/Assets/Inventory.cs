d5c95fe855b8dbb5e91b99ce8ba8f860a2a4b6ed
ryancairns107 <ryan.cairns.107@gmail.com>
2019-09-26 14:59:06 +0200

    +inventory
    inventory and pickups

---
+++ GDY2P1Group1/Assets/Inventory.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public List<Item> itemList = new List<Item>();
+    public int maxInventorySize;
+    public int selectedItem;
+    private GameObject go;
+    private void Start()
+    {
+        go = FindObjectOfType<CharacterMovement>().gameObject;
+    }
+
+    public void AddItem(Item item)
+    {
+        if (itemList.Count < maxInventorySize)
+        {
+            itemList.Add(item);
+        }
+        else
+        {
+            DropSelectedItem();
+            itemList.Insert(selectedItem-1, item);
+        }
+
+
+    }
+
+    public void DropSelectedItem()
+    {
+        Instantiate(itemList[selectedItem - 1].parentObject, go.transform.position, go.transform.rotation);
+        RemoveItem(itemList[selectedItem - 1]);
+    }
+
+    public void RemoveItem(Item item)
+    {
+        itemList.Remove(item);
+    }
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selectedItem = 1;
+            Debug.Log(selectedItem);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (itemList.Count >= 2)
+            {
+                selectedItem = 2;
+                Debug.Log(selectedItem);
+            }
+            else
+            {
+                selectedItem = 1;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            if (itemList.Count == 3)
+            {
+                selectedItem = 3;
+                Debug.Log(selectedItem);
+            }
+            else
+            {
+                selectedItem = itemList.Count;
+            }
+        }
+        if (itemList.Count > 0)
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                DropSelectedItem();
+                if (selectedItem > itemList.Count)
+                {
+                    selectedItem = itemList.Count;
+                }
+            }
+        if(selectedItem == 0 && itemList.Count > 0)
+        {
+            selectedItem = 1;
+        }
+        //if (itemList[selectedItem-1] == Throwable){ //throwItem
+        //if (Input.GetMouseButtonDown(1)) //right click
+        //{
+            //DropSelectedItem & rigidBody addforce (25/weight)
+        //}
+        //}
+    }
+}
---
+++ GDY2P1Group1/Assets/Scripts/ThrowableItem.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class ThrowableItem : Item
+{
+    public int damage;
+    public int weight;
+}
